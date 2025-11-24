9676ef99348e579bfb16ccef4542d7b46de312c7
aliftheprogramer <alifarya679@gmail.com>
2025-11-24 10:23:33 +0700

    kwefiwufe

--- lib/widget/bottom_navigator_widget.dart
+++ lib/widget/bottom_navigator_widget.dart
@@ -12,46 +12,32 @@ class BottomNavigatorWidget extends StatelessWidget {
     return BlocBuilder<NavigationCubit, NavigationState>(
       builder: (context, state) {
         return Container(
-          // Memberikan sedikit bayangan (shadow) di atas agar terlihat manis
           decoration: const BoxDecoration(
             boxShadow: [
               BoxShadow(color: Colors.black12, blurRadius: 4, spreadRadius: 1),
             ],
           ),
           child: BottomNavigationBar(
-            // Mengatur tipe agar teks selalu muncul jika diperlukan
             type: BottomNavigationBarType.fixed,
-            // Menghilangkan garis batas default di atas bottom bar
             elevation: 0,
             backgroundColor: Colors.white,
-
-            // Mengambil index dari State Cubit kita
             currentIndex: state.index,
-
-            // Warna saat item dipilih (Biru sesuai gambar)
             selectedItemColor: Colors.blue,
-            // Warna saat item tidak dipilih (Abu-abu/Biru tua pudar)
             unselectedItemColor: Colors.blueGrey,
-
-            // Ukuran font label
             selectedLabelStyle: const TextStyle(
               fontSize: 12,
               fontWeight: FontWeight.w500,
             ),
             unselectedLabelStyle: const TextStyle(fontSize: 12),
 
-            // Fungsi yang dijalankan saat item diklik
             onTap: (index) {
               context.read<NavigationCubit>().getNavBarItem(index);
             },
 
-            // Daftar Menu
             items: const [
               BottomNavigationBarItem(
                 icon: Icon(Icons.home_outlined),
-                activeIcon: Icon(
-                  Icons.home,
-                ), // Ikon saat aktif (opsional bisa dibuat solid)
+                activeIcon: Icon(Icons.home),
                 label: 'Beranda',
               ),
               BottomNavigationBarItem(
